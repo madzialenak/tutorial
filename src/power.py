836aeb85d5d86514b79fc9b2febd34209a4d7816
Magdalena Kawka <magdalena.kawka@mycit.ie>
2017-09-29 13:39:08 +0200

    made speculative changes

--- src/power.py
+++ src/power.py
@@ -1,12 +1,15 @@
 """ This is a script that calculates one number to the power of another.
 """
 def to_the_power(x, y = 2):
-	result = x
-	for i in range(0, y):
-		result = result * x
-	return result
+	""" trying this out
+	"""
+	return x ** y
+#	result = x
+#	for i in range(0, y):
+#		result = result * x
+#	return result
 
-x = 9
-y = 8
+x = 5  #
+y = 8  #
 print ("{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y)))
 print ("The End")
